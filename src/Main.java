a56980d2dfcab094c7ed8bda2c76855472d40bf1
DavidHask <haskeldd@mail.uc.edu>
2024-09-20 11:54:30 -0400

    Lab 4, fourth task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int testNumber = 7;
+        int testModulo = testNumber % 2;
+
+        if (testModulo == 1) {
+            System.out.println("\nBecause the remainder of the modulo" +
+                    " function is " + testModulo + ", " + testNumber +
+                    " is odd");
+        }  else {
+            System.out.println("\nBecause the remainder of the modulo" +
+                    " function is " + testModulo + ", " + testNumber +
+                    " is even");
+        }
     }
 }
